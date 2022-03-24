1b0c85c3b47e14ff7d8e7e012df872a47960a809
Dylan Delobel <dylan.delobel66@gmail.com>
2022-03-24 20:42:30 +0100

    Update login.js

--- login.js
+++ login.js
@@ -7,9 +7,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe("Login into popcarte", () => {
     it("Open the login page", () => {
         cy.visit("https://popcarte.com/login.php")
-
         cy.get("#onetrust-accept-btn-handler").click()
-
         cy.get('#ulogin').type('dylan@pop.com')
         cy.get('#loginForm-submit-button').click()
         cy.contains("Bonjour").should("Bonjour, Yanik")
