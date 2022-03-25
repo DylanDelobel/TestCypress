2209e031ab11169d12059a62aed5763f88ad0150
Dylan Delobel <dylan.delobel66@gmail.com>
2022-03-25 13:13:42 +0100

    Update homepage.js

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,9 +1,3 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-})
-
 describe("Login into popcarte", () => {
     it("Open the login page", () => {
         cy.visit("https://popcarte.com/login.php")
